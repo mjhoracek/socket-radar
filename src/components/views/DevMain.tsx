6e8e50f430e72eb00a293b03e01bc2da5d5f142e
mjhoracek <mitch@inthezone.dev>
2024-01-18 22:10:22 -0700

    pitch spawner

---
+++ src/components/views/DevMain.tsx
@@ -0,0 +1,35 @@
+import { useState } from "react";
+import { Button } from "../shared/Button";
+
+export const DevMain = () => {
+  console.log(import.meta.env);
+  const [data, setData] = useState<number | null>(null);
+
+  const spawnPitch = () => {
+    // Generate a random number between 69.0 and 110.0
+    const randomNumber = parseFloat(
+      (Math.floor(Math.random() * 1001) / 10).toFixed(1)
+    );
+    setData(randomNumber);
+  };
+
+  return (
+    <div className="flex flex-col justify-center items-center w-screen h-screen bg-appBg relative">
+      <div className="flex flex-col top-5 right-10 absolute ">
+        <Button label={"spawn pitch"} onClick={() => spawnPitch()} />
+        <Button label={"reset"} onClick={() => setData(null)} />
+      </div>
+      <p
+        className="text-veloColor"
+        style={{
+          fontFamily: "segment, sans serif",
+          fontWeight: "bold",
+          lineHeight: "80%",
+          fontSize: "40vw",
+        }}
+      >
+        {data ? data : "---.-"}
+      </p>
+    </div>
+  );
+};
--- src/components/views/MainView.tsx
+++ src/components/views/MainView.tsx
@@ -1,36 +1,201 @@
-import React, { useEffect, useState } from "react";
-import { Button } from "@mantine/core";
+import { useEffect, useState } from "react";
+import { Group, Select } from "@mantine/core";
+import { closeAllModals, openModal } from "@mantine/modals";
+import { Button } from "../shared/Button";
+import { SVG } from "../shared/SVG";
+import { UpdateButton } from "../update";
 
 type Props = {};
 
 const MainView = (props: Props) => {
-  console.log(window.ipcRenderer);
-
+  console.log(import.meta.env);
   const [data, setData] = useState(null);
-  const [error, setError] = useState(null);
+  const [error, setError] = useState<string | null>(null);
+  const [status, setStatus] = useState(null);
+  const [ports, setPorts] = useState([] as any[]);
+  const [selectedPort, setSelectedPort] = useState<string | null>(null);
+
+  const activatePort = (port: string) => {
+    setSelectedPort(port);
+    setData(null);
+    if (window.Main) {
+      window.Main.sendMessage("activatePort", port);
+    }
+    setError(null);
+    closeAllModals();
+  };
+
+  const openPortModal = () => {
+    window.Main.sendMessage("closePort", "closePort");
+    setSelectedPort(null);
+    setData(null);
+    setError(null);
+    window.Main.sendMessage("getPorts", "getPorts");
+    openModal({
+      title: "Select a port",
+      children: (
+        <Select
+          placeholder="Select a port"
+          onChange={(e: string) => activatePort(e)}
+          data={
+            ports
+              ? ports.map((port: any) => {
+                  return { label: port.friendlyName, value: port.path };
+                })
+              : []
+          }
+        />
+      ),
+    });
+  };
 
   useEffect(() => {
     if (window.Main) {
       window.Main.on("ping", (data: any) => {
+        setError(null);
         setData(data);
+      });
+
+      window.Main.on("error", (data: any) => {
+        if (data === "Data Flow Interrupted") {
+          setData(null);
+        }
+        setError(data);
+      });
+
+      window.Main.on("status", (data: any) => {
+        if (data === "Port Closed") {
+          setData(null);
+        }
+        setStatus(data);
       });
     }
   }, []);
 
   useEffect(() => {
-    window.Main.on("error", (data: any) => {
-      setError(data);
+    window.Main.on("ports", (data: any) => {
+      setPorts(data);
+      openModal({
+        closeOnClickOutside: true,
+        title: "Select a port",
+        children: (
+          <>
+            {data.length === 0 ? (
+              <p>
+                No ports found - try connecting your usb and click SELECT PORT
+              </p>
+            ) : (
+              <Select
+                placeholder="Select a port"
+                onChange={(e: string) => activatePort(e)}
+                data={
+                  data
+                    ? data.map((port: any) => {
+                        return { label: port.friendlyName, value: port.path };
+                      })
+                    : []
+                }
+              />
+            )}
+          </>
+        ),
+      });
     });
   }, []);
 
-  const reconnectPort = () => {
-    if (window.Main) {
-      window.Main.sendMessage("reconnect", "reconnect");
-    }
-  };
   return (
-    <div className="bg-red-800">
-      <p>content</p>
+    <div className="flex flex-col justify-center items-center w-screen h-screen bg-appBg relative">
+      <div className="flex flex-row top-5 left-10 absolute ">
+        <div className="flex flex-row justify-center items-center">
+          <Button
+            label={
+              selectedPort ? (
+                <Group px={10}>
+                  {data && status === "Port Open" ? (
+                    <span className="relative flex h-3 w-3">
+                      <span className="animate-ping absolute inline-flex h-full w-full rounded-full bg-green-600 opacity-75"></span>
+                      <span className="relative inline-flex rounded-full h-3 w-3 bg-green-600"></span>
+                    </span>
+                  ) : (
+                    <SVG.X size={20} color="red" />
+                  )}
+                  <p>{`${selectedPort}`}</p>
+                </Group>
+              ) : (
+                <div className="flex justify-start">
+                  <p>Select Port</p>
+                </div>
+              )
+            }
+            onClick={openPortModal}
+          />
+          <div className="flex justify-center items-center px-2  leading-[0px]">
+            <p>{error}</p>
+          </div>
+        </div>
+      </div>
+      <div className="flex flex-row top-5 right-10 absolute ">
+        <UpdateButton />
+      </div>
+      {selectedPort && (
+        <>
+          {data === `   . ` ? (
+            <p
+              className="text-veloColor"
+              style={{
+                fontFamily: "segment, sans serif",
+                fontWeight: "bold",
+                lineHeight: "80%",
+                fontSize: "40vw",
+              }}
+            >
+              {"---.-"}
+            </p>
+          ) : data ? (
+            <p
+              className="text-veloColor"
+              style={{
+                fontFamily: "segment, sans serif",
+                fontWeight: "bold",
+                lineHeight: "80%",
+                fontSize: "40vw",
+              }}
+            >
+              {`${data}`}
+            </p>
+          ) : (
+            <div>
+              <p
+                style={{
+                  fontFamily: "segment, sans serif",
+                  fontWeight: "bold",
+                  lineHeight: "80%",
+                  fontSize: "10vw",
+                }}
+              >
+                {"No Data Flow"}
+              </p>
+              <p className="text-2xl">
+                Make sure your gun is in Transmit Mode (XMIT){" "}
+              </p>
+            </div>
+          )}
+        </>
+      )}
+      {!data && !selectedPort && (
+        <div>
+          <p
+            style={{
+              fontFamily: "segment, sans serif",
+              fontWeight: "bold",
+              lineHeight: "80%",
+              fontSize: "10vw",
+            }}
+          >
+            {"Not Connected"}
+          </p>
+        </div>
+      )}
     </div>
   );
 };
